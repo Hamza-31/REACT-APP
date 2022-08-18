a3c4f158924e5197280902ae707dc5d5e1059f04
Hamza <hamza.labzioui@gmail.com>
2022-08-18 15:49:34 +0200

    first commit

---
+++ src/hooks/pokemon.hook.ts
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import Pokemon from '../models/pokemon';
+import POKEMONS from '../models/mock-pokemon';
+
+const usePokemons = () => {
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/pokemons')
+            .then(response => response.json())
+            .then((pokemons => {
+                setPokemons(pokemons)
+            }))
+    }, []);
+    return pokemons;
+}
+
+export default usePokemons;
